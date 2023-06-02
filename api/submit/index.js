5b4cb3c810fe4c9c30b0f2fedec01eb54547ee9d
ykundesu <49261069+ykundesu@users.noreply.github.com>
2023-06-02 18:44:18 +0900

    Add files via upload

--- api/submit/index.js
+++ api/submit/index.js
@@ -20,7 +20,9 @@ const send = o => webpush.sendNotification(o.subscription, JSON.stringify(o.payl
 })
 
 app.post('/api/submit', cors(), async (req, res) => {
-  request.post({url:'https://sssokuhoupusher-1-b0836240.deta.app/', form:req.body}, (error, response, body) => {res.json(req.body)});
+  request.post({url:'https://sssokuhoupusher-1-b0836240.deta.app/', headers: {
+    "content-type": "application/json"
+  },body:JSON.stringify(req.body)}, (error, response, body) => {res.json(body)});
   //res.json(await send(req.body))
 })
 
