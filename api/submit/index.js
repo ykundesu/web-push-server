c458d16227bb281470483275f959072f61ac00b1
ykundesu <49261069+ykundesu@users.noreply.github.com>
2023-06-02 17:44:48 +0900

    Add files via upload

--- api/submit/index.js
+++ api/submit/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-const request = require('request');
+import request from 'request'
 const app = express()
 app.use(cors())
 app.use(express.json({ type: '*/*' }))
